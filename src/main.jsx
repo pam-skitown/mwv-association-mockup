83cb2b59a30c5417625f2808aac3cad0eb1daf14
pam-skitown <pam@skitown.realty>
2026-05-13 16:14:00 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MWVMockSite from '../App.jsx';
+
+createRoot(document.getElementById('root')).render(<MWVMockSite />);
